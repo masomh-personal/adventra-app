81d8aaca6bc1436bdbd1d688a40d43e6ce8805e6
masomh-personal <masom@hamadeh.io>
2025-03-16 20:14:32 -0400

    Updated way to get fonts from Google. This is optimized for Next.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import '@/styles/globals.css';
+import { Montserrat, Lexend } from 'next/font/google';
+
+// Load fonts with specific weights and attach to CSS variables
+const headingFont = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-heading',
+});
+
+const bodyFont = Lexend({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-body',
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={`${headingFont.variable} ${bodyFont.variable} flex flex-col min-h-screen`}>
       <Header />
       <main className="flex-grow">
         <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Fonts: Montserrat for headings and Lexend for body */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Lexend:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head>{/* Fonts are loaded via next/font in _app.js */}</Head>
       <body>
         <Main />
         <NextScript />
