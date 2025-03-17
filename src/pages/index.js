d658657d2995fd57f2d10adcb3032396a67b643b
masomh-personal <masom@hamadeh.io>
2025-03-16 21:21:42 -0400

    Cleaned up import section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Button from '@/components/Button';
 import { useRouter } from 'next/router';
+import Button from '@/components/Button';
 
 export default function HomePage() {
   const router = useRouter(); // Initialize router
