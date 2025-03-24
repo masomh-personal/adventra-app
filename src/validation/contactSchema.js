93fdbf948f15ef2b1fde3b54c29f97248b334965
masomh-personal <masom@hamadeh.io>
2025-03-23 21:03:20 -0400

    Added a shared validation schema/utils file to share across the app. Following DRY principles and for future scalability

--- src/validation/contactSchema.js
+++ src/validation/contactSchema.js
@@ -1,14 +1,8 @@
 import * as yup from 'yup';
+import { nameValidation, emailValidation, messageValidation } from './validationUtils';
 
-// Validation schema for the contact form
 export const contactFormSchema = yup.object().shape({
-  name: yup.string().required('Please enter your name'),
-  email: yup
-    .string()
-    .email('Please enter a valid email address')
-    .required('Please enter your email'),
-  message: yup
-    .string()
-    .required('Please enter your message')
-    .min(10, 'Message must be at least 10 characters'),
+  name: nameValidation,
+  email: emailValidation,
+  message: messageValidation,
 });
--- src/validation/loginSchema.js
+++ src/validation/loginSchema.js
@@ -1,20 +1,8 @@
 import * as yup from 'yup';
+import { emailValidation, passwordValidation } from './validationUtils';
 
 export const loginSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email('Please enter a valid email address')
-    .required('Email is required')
-    .transform((value) => value.toLowerCase()), // Normalize email to lowercase
-
-  password: yup
-    .string()
-    .min(10, 'Password must be at least 10 characters')
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-      'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character'
-    )
-    .required('Password is required'),
-
+  email: emailValidation,
+  password: passwordValidation,
   rememberMe: yup.boolean().optional(),
 });
