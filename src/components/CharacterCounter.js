5eb93c0ea3c42585585f1b7676b5bbd9d2a70729
masomh-personal <masom@hamadeh.io>
2025-04-06 23:54:05 -0400

    style(CharacterCounter): made text a bit clearer

--- src/components/CharacterCounter.js
+++ src/components/CharacterCounter.js
@@ -13,7 +13,7 @@ export function CharacterCounter({ value = '', maxLength }) {
 
   return (
     <div
-      className={`text-xs text-right mt-1 ${
+      className={`text-xs font-heading font-bold text-right mt-1 ${
         isAtLimit ? 'text-red-500 font-bold' : isNearLimit ? 'text-amber-500' : 'text-green-600'
       }`}
       data-testid="char-counter"
